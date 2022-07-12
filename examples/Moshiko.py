c6e881440d430bc066aef11bf3429b30237b163e
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 13:57:22 +0300

    Moshiko 3

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -10,22 +10,28 @@ def drive(world):
     obstacle0 = world.get((0, y - 2))
     obstacle1 = world.get((1, y - 2))
     obstacle2 = world.get((2, y - 2))
-    if world.get((x, y - 1)) == obstacles.PENGUIN:
-        return actions.PICKUP
-    if obstacle0 == obstacles.PENGUIN:
-        if x == 0:
-            return actions.NONE
-        if x == 1:
-            return actions.LEFT
-    if obstacle1 == obstacles.PENGUIN:
-        if x == 0:
-            return actions.RIGHT
-        if x == 1:
-            return actions.NONE
-        if x == 2:
-            return actions.LEFT
-    if obstacle2 == obstacles.PENGUIN:
-        if x == 1:
-            return actions.RIGHT
-        if x == 2:
-            return actions.NONE
+
+    def use(obstacle, use_action):
+        if world.get((x, y - 1)) == obstacle:
+            return use_action
+        if obstacle0 == obstacle:
+            if x == 0:
+                return actions.NONE
+            if x == 1:
+                return actions.LEFT
+        if obstacle1 == obstacle:
+            if x == 0:
+                return actions.RIGHT
+            if x == 1:
+                return actions.NONE
+            if x == 2:
+                return actions.LEFT
+        if obstacle2 == obstacle:
+            if x == 1:
+                return actions.RIGHT
+            if x == 2:
+                return actions.NONE
+    use(obstacles.PENGUIN, actions.PICKUP)
+    use(obstacles.WATER, actions.BRAKE)
+    use(obstacles.CRACK, actions.JUMP)
+    return actions.NONE
