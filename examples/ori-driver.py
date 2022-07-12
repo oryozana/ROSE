9dafdf5b747bf907ea0071ce67d4f0eaf1eeb21d
oryozana <oryozana@gmail.com>
2022-07-12 13:34:08 +0300

    ori-driver

--- examples/ori-driver.py
+++ examples/ori-driver.py
@@ -51,21 +51,21 @@ def drive(world):
     if obstacle2 == obstacles.WATER or obstacle2 == obstacles.CRACK:
         return actions.NONE
 
-    if x == 0 + side or x == 1 + side:
-        obstacle2 = world.get((x + 1, y - 2))
-        if obstacle2 == obstacles.WATER or obstacle2 == obstacles.CRACK:
-            return actions.RIGHT
-
-    if x == 2 + side or x == 1 + side:
-        obstacle2 = world.get((x - 1, y - 2))
-        if obstacle2 == obstacles.WATER or obstacle2 == obstacles.CRACK:
-            return actions.LEFT
-
     if x == 0 + side and world.get((x + 1, y - 1)) == obstacles.NONE:
         return actions.RIGHT
 
     if x == 2 + side and world.get((x - 1, y - 1)) == obstacles.NONE:
         return actions.LEFT
+
+    if x == 0 + side or x == 1 + side:
+        obstacle2 = world.get((x + 1, y - 1))
+        if obstacle2 == obstacles.WATER or obstacle2 == obstacles.CRACK:
+            return actions.RIGHT
+
+    if x == 2 + side or x == 1 + side:
+        obstacle2 = world.get((x - 1, y - 1))
+        if obstacle2 == obstacles.WATER or obstacle2 == obstacles.CRACK:
+            return actions.LEFT
 
     return actions.NONE
 
