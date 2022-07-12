c7c4616fd82be27032d8bbee4abb3641fffadaef
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 13:16:23 +0300

    Moshiko 1

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -1,1 +1,10 @@
-hello
+import random
+from rose.common import obstacles, actions  # NOQA
+
+driver_name = "Moshiko Boozaglo"
+
+
+def drive(world):
+    obstacle = world.get(x, y)
+    if obstacle == obstacles.PENGUIN:
+        actions.PICKUP
