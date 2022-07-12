41fc01d453e2ce23fa4393df0bfd05c636466208
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 15:01:15 +0300

    Moshiko 3

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -7,36 +7,29 @@ driver_name = "Moshiko Boozaglo"
 # def use(x, y, front, obstacle0, obstacle1, obstacle2, obstacle, use_action):
 
 
-
-
 def drive(world):
     x = world.car.x
     y = world.car.y
     obstacle0 = world.get((0, y - 2))
     obstacle1 = world.get((1, y - 2))
     obstacle2 = world.get((2, y - 2))
-    obstacle = obstacles.PENGUIN
-    use_action = actions.PICKUP
-    if world.get((x, y - 1)) == obstacle:
-        return use_action
-    if obstacle0 == obstacle:
+    if world.get((x, y - 1)) == obstacles.PENGUIN:
+        return actions.PICKUP
+    if obstacle0 == obstacles.PENGUIN:
         if x == 0:
             return actions.NONE
         if x == 1:
             return actions.LEFT
-    if obstacle1 == obstacle:
+    if obstacle1 == obstacles.PENGUIN:
         if x == 0:
             return actions.RIGHT
         if x == 1:
             return actions.NONE
         if x == 2:
             return actions.LEFT
-    if obstacle2 == obstacle:
+    if obstacle2 == obstacles.PENGUIN:
         if x == 1:
             return actions.RIGHT
         if x == 2:
             return actions.NONE
-    #use(x, y, world.get((x, y - 1)), obstacle0, obstacle1, obstacle2, obstacles.PENGUIN, actions.PICKUP)
-    # use(obstacles.WATER, actions.BRAKE)
-    # use(obstacles.CRACK, actions.JUMP)
     return actions.NONE
