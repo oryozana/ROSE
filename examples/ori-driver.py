88dc35a0f1edebdab367ab31a498032bd46fcfca
oryozana <oryozana@gmail.com>
2022-07-12 15:01:14 +0300

    ori-driver

--- examples/ori-driver.py
+++ examples/ori-driver.py
@@ -3,6 +3,14 @@ from rose.common import obstacles, actions
 driver_name = "Ori Driver"
 
 barriers = (obstacles.BARRIER, obstacles.BIKE, obstacles.TRASH)
+obstacles_dict = {obstacles.NONE: 0,
+                  obstacles.TRASH: -10,
+                  obstacles.BIKE: -10,
+                  obstacles.BARRIER: -10,
+                  obstacles.CRACK: 5,
+                  obstacles.WATER: 4,
+                  obstacles.PENGUIN: 10}
+
 
 def drive(world):
     x = world.car.x
@@ -31,12 +39,18 @@ def drive(world):
     if obstacle1 in barriers:
         if x == 1 + side:
             left_obstacle1 = world.get((x - 1, y - 1))
+            left_obstacle2 = world.get((x - 1, y - 2))
             left_obstacle3 = world.get((x - 1, y - 3))
             right_obstacle1 = world.get((x + 1, y - 1))
+            right_obstacle2 = world.get((x + 1, y - 2))
             right_obstacle3 = world.get((x + 1, y - 3))
-            if left_obstacle1 == obstacles.NONE and left_obstacle3 == obstacles.PENGUIN:
+
+            left_sum = obstacles_dict[left_obstacle2] + obstacles_dict[left_obstacle3]
+            right_sum = obstacles_dict[right_obstacle2] + obstacles_dict[right_obstacle3]
+
+            if left_obstacle1 == obstacles.NONE and left_sum >= right_sum:
                 return actions.LEFT
-            if right_obstacle1 == obstacles.NONE and right_obstacle3 == obstacles.PENGUIN:
+            if right_obstacle1 == obstacles.NONE and right_sum > left_sum:
                 return actions.RIGHT
 
         if x == 2 + side or x == 1 + side:
