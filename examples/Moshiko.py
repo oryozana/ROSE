1410079bc8d4e345bf5a22ea544ef6b9a0ec50ef
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 13:24:35 +0300

    Moshiko 1

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -5,6 +5,8 @@ driver_name = "Moshiko Boozaglo"
 
 
 def drive(world):
+    x = world.car.x
+    y = world.car.y
     obstacle = world.get(x, y)
     if obstacle == obstacles.PENGUIN:
         return actions.PICKUP
