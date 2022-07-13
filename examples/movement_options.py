8a41db3b8728e544561d476e76284a2339ebcc69
oryozana <oryozana@gmail.com>
2022-07-13 13:57:15 +0300

    ori-driver

---
+++ examples/movement_options.py
@@ -0,0 +1,369 @@
+
+left1 = [[True, True, True, True, True],
+        [False, False, False, False, False],
+        [False, False, False, False, False]]
+
+left2 = [[True, True, True, True, False],
+        [False, False, False, False, True],
+        [False, False, False, False, False]]
+
+left3 = [[True, True, True, False, True],
+        [False, False, False, True, False],
+        [False, False, False, False, False]]
+
+left4 = [[True, True, True, False, False],
+        [False, False, False, True, True],
+        [False, False, False, False, False]]
+
+left5 = [[True, True, True, False, False],
+        [False, False, False, True, False],
+        [False, False, False, False, True]]
+
+left6 = [[True, True, False, True, True],
+        [False, False, True, False, False],
+        [False, False, False, False, False]]
+
+left7 = [[True, True, False, True, False],
+        [False, False, True, False, True],
+        [False, False, False, False, False]]
+
+left8 = [[True, True, False, False, True],
+        [False, False, True, True, False],
+        [False, False, False, False, False]]
+
+left9 = [[True, True, False, False, False],
+        [False, False, True, True, True],
+        [False, False, False, False, False]]
+
+left10 = [[True, True, False, False, False],
+        [False, False, True, True, False],
+        [False, False, False, False, True]]
+
+left11 = [[True, True, False, False, False],
+        [False, False, True, False, True],
+        [False, False, False, True, False]]
+
+left12 = [[True, True, False, False, False],
+        [False, False, True, False, False],
+        [False, False, False, True, True]]
+
+left13 = [[True, False, True, True, True],
+        [False, True, False, False, False],
+        [False, False, False, False, False]]
+
+left14 = [[True, False, True, True, False],
+        [False, True, False, False, True],
+        [False, False, False, False, False]]
+
+left15 = [[True, False, True, False, True],
+        [False, True, False, True, False],
+        [False, False, False, False, False]]
+
+left16 = [[True, False, True, False, False],
+        [False, True, False, True, True],
+        [False, False, False, False, False]]
+
+left17 = [[True, False, True, False, False],
+        [False, True, False, True, False],
+        [False, False, False, False, True]]
+
+left18 = [[True, False, False, False, False],
+        [False, True, True, True, False],
+        [False, False, False, False, True]]
+
+left19 = [[True, False, False, False, False],
+        [False, True, True, False, True],
+        [False, False, False, True, False]]
+
+left20 = [[True, False, False, False, False],
+        [False, True, True, False, False],
+        [False, False, False, True, True]]
+
+left21 = [[True, False, False, False, True],
+        [False, True, False, True, False],
+        [False, False, True, False, False]]
+
+left22 = [[True, False, False, False, False],
+        [False, True, False, True, True],
+        [False, False, True, False, False]]
+
+left23 = [[True, False, False, False, False],
+        [False, True, False, True, False],
+        [False, False, True, False, True]]
+
+left24 = [[True, False, False, False, False],
+        [False, True, False, False, True],
+        [False, False, True, True, False]]
+
+left25 = [[True, False, False, False, False],
+        [False, True, False, False, False],
+        [False, False, True, True, True]]
+
+
+right1 = [[False, False, False, False, False],
+        [False, False, False, False, False],
+        [True, True, True, True, True]]
+
+right2 = [[False, False, False, False, False],
+        [False, False, False, False, True],
+        [True, True, True, True, False]]
+
+right3 = [[False, False, False, False, False],
+        [False, False, False, True, False],
+        [True, True, True, False, True]]
+
+right4 = [[False, False, False, False, False],
+        [False, False, False, True, True],
+        [True, True, True, False, False]]
+
+right5 = [[False, False, False, False, True],
+        [False, False, False, True, False],
+        [True, True, True, False, False]]
+
+right6 = [[False, False, False, False, False],
+        [False, False, True, False, False],
+        [True, True, False, True, True]]
+
+right7 = [[False, False, False, False, False],
+        [False, False, True, False, True],
+        [True, True, False, True, False]]
+
+right8 = [[False, False, False, False, False],
+        [False, False, True, True, False],
+        [True, True, False, False, True]]
+
+right9 = [[False, False, False, False, False],
+        [False, False, True, True, True],
+        [True, True, False, False, False]]
+
+right10 = [[False, False, False, False, False],
+        [False, False, True, True, False],
+        [True, True, False, False, True]]
+
+right11 = [[False, False, False, True, False],
+        [False, False, True, False, True],
+        [True, True, False, False, False]]
+
+right12 = [[False, False, False, True, True],
+        [False, False, True, False, False],
+        [True, True, False, False, False]]
+
+right13 = [[False, False, False, False, False],
+        [False, True, False, False, False],
+        [True, False, True, True, True]]
+
+right14 = [[False, False, False, False, False],
+        [False, True, False, False, True],
+        [True, False, True, True, False]]
+
+right15 = [[False, False, False, False, False],
+        [False, True, False, True, False],
+        [True, False, True, False, True]]
+
+right16 = [[False, False, False, False, False],
+        [False, True, False, True, True],
+        [True, False, True, False, False]]
+
+right17 = [[False, False, False, False, True],
+        [False, True, False, True, False],
+        [True, False, True, False, False]]
+
+right18 = [[False, False, False, False, True],
+        [False, True, True, True, False],
+        [True, False, False, False, False]]
+
+right19 = [[False, False, False, True, False],
+        [False, True, True, False, True],
+        [True, False, False, False, False]]
+
+right20 = [[False, False, False, True, True],
+        [False, True, True, False, False],
+        [True, False, False, False, False]]
+
+right21 = [[False, False, True, False, False],
+        [False, True, False, True, False],
+        [True, False, False, False, True]]
+
+right22 = [[False, False, True, False, False],
+        [False, True, False, True, True],
+        [True, False, False, False, False]]
+
+right23 = [[False, False, True, False, True],
+        [False, True, False, True, False],
+        [True, False, False, False, False]]
+
+right24 = [[False, False, True, True, False],
+        [False, True, False, False, True],
+        [True, False, False, False, False]]
+
+right25 = [[False, False, True, True, True],
+        [False, True, False, False, False],
+        [True, False, False, False, False]]
+
+
+mid1 = [[False, False, False, False, False],
+        [True, True, True, True, True],
+        [False, False, False, False, False]]
+
+mid2 = [[False, False, False, False, True],
+        [True, True, True, True, False],
+        [False, False, False, False, False]]
+
+mid3 = [[False, False, False, False, False],
+        [True, True, True, True, False],
+        [False, False, False, False, True]]
+
+mid4 = [[False, False, False, True, True],
+        [True, True, True, False, False],
+        [False, False, False, False, False]]
+
+mid5 = [[False, False, False, True, False],
+        [True, True, True, False, True],
+        [False, False, False, False, False]]
+
+mid6 = [[False, False, False, False, False],
+        [True, True, True, False, True],
+        [False, False, False, True, False]]
+
+mid7 = [[False, False, False, False, False],
+        [True, True, True, False, False],
+        [False, False, False, True, True]]
+
+mid8 = [[False, False, True, True, True],
+        [True, True, False, False, False],
+        [False, False, False, False, False]]
+
+mid9 = [[False, False, True, True, False],
+        [True, True, False, False, True],
+        [False, False, False, False, False]]
+
+mid10 = [[False, False, True, False, True],
+        [True, True, False, True, False],
+        [False, False, False, False, False]]
+
+mid11 = [[False, False, True, False, False],
+        [True, True, False, True, True],
+        [False, False, False, False, False]]
+
+mid12 = [[False, False, True, False, False],
+        [True, True, False, True, False],
+        [False, False, False, False, True]]
+
+mid13 = [[False, False, False, False, False],
+        [True, True, False, True, True],
+        [False, False, True, False, False]]
+
+mid14 = [[False, False, False, False, False],
+        [True, True, False, True, False],
+        [False, False, True, False, True]]
+
+mid15 = [[False, False, False, False, False],
+        [True, True, False, False, True],
+        [False, False, True, True, False]]
+
+mid16 = [[False, False, False, False, False],
+        [True, True, False, False, False],
+        [False, False, True, True, True]]
+
+mid17 = [[False, True, True, True, True],
+        [True, False, False, False, False],
+        [False, False, False, False, False]]
+
+mid18 = [[False, True, True, True, False],
+        [True, False, False, False, True],
+        [False, False, False, False, False]]
+
+mid19 = [[False, True, True, False, True],
+        [True, False, False, True, False],
+        [False, False, False, False, False]]
+
+mid20 = [[False, True, True, False, False],
+        [True, False, False, True, True],
+        [False, False, False, False, False]]
+
+mid21 = [[False, True, True, False, False],
+        [True, False, False, True, False],
+        [False, False, False, False, True]]
+
+mid22 = [[False, True, False, True, True],
+        [True, False, True, False, False],
+        [False, False, False, False, False]]
+
+mid23 = [[False, True, False, True, False],
+        [True, False, True, False, True],
+        [False, False, False, False, False]]
+
+mid24 = [[False, True, False, False, True],
+        [True, False, True, True, False],
+        [False, False, False, False, False]]
+
+mid25 = [[False, True, False, False, False],
+        [True, False, True, True, True],
+        [False, False, False, False, False]]
+
+mid26 = [[False, True, False, False, False],
+        [True, False, True, True, False],
+        [False, False, False, False, True]]
+
+mid27 = [[False, True, False, False, False],
+        [True, False, True, False, True],
+        [False, False, False, True, False]]
+
+mid28 = [[False, True, False, False, False],
+        [True, False, True, False, False],
+        [False, False, False, True, True]]
+
+mid29 = [[False, False, False, True, True],
+        [True, False, True, False, False],
+        [False, True, False, False, False]]
+
+mid30 = [[False, False, False, True, False],
+        [True, False, True, False, True],
+        [False, True, False, False, False]]
+
+mid31 = [[False, False, False, False, True],
+        [True, False, True, True, False],
+        [False, True, False, False, False]]
+
+mid32 = [[False, False, False, False, False],
+        [True, False, True, True, True],
+        [False, True, False, False, False]]
+
+mid33 = [[False, False, False, False, False],
+        [True, False, True, True, False],
+        [False, True, False, False, True]]
+
+mid34 = [[False, False, False, False, False],
+        [True, False, True, False, True],
+        [False, True, False, True, False]]
+
+mid35 = [[False, False, False, False, False],
+        [True, False, True, False, False],
+        [False, True, False, True, True]]
+
+mid36 = [[False, False, False, False, False],
+        [True, False, False, True, True],
+        [False, True, True, False, False]]
+
+mid37 = [[False, False, False, False, False],
+        [True, False, False, True, False],
+        [False, True, True, False, True]]
+
+mid38 = [[False, False, False, False, False],
+        [True, False, False, False, True],
+        [False, True, True, True, False]]
+
+mid39 = [[False, False, False, False, False],
+        [True, False, False, False, False],
+        [False, True, True, True, True]]
+
+left_moves = [left1, left2, left3, left4, left5, left6, left7, left8, left9, left10, left11, left12, left13,
+              left14, left15, left16, left17, left18, left19, left20, left21, left22, left23, left24, left25]
+
+right_moves = [right1, right2, right3, right4, right5, right6, right7, right8, right9, right10, right11, right12,
+               right13, right14, right15, right16, right17, right18, right19, right20, right21, right22, right23,
+               right24, right25]
+
+middle_moves = [mid1, mid2, mid3, mid4, mid5, mid6, mid7, mid8, mid9, mid10, mid11, mid12, mid13, mid14, mid15,
+                mid16, mid17, mid14, mid15, mid16, mid17, mid18, mid19, mid20, mid21, mid22, mid23, mid24, mid25,
+                mid26, mid27, mid28, mid29, mid30, mid31, mid32, mid33, mid34, mid35, mid36, mid37, mid38, mid39]
