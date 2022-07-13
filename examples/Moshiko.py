dd20f3862b81c7ee6ad47484dda2a51ef1516295
BuildTools <unconfigured@null.spigotmc.org>
2022-07-13 14:12:18 +0300

    Hello

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -6,13 +6,14 @@ driver_name = "Moshiko Boozaglo"
 
 # def use(x, y, front, obstacle0, obstacle1, obstacle2, obstacle, use_action):
 
+
 USABLES = [obstacles.PENGUIN, obstacles.WATER, obstacles.CRACK]
 ACTIONS = [actions.PICKUP, actions.BRAKE, actions.JUMP]
 def drive(world):
     
     x = world.car.x
     y = world.car.y
-    obst = world.get(x, y - 2)
+    obst = world.get((x, y - 2))
     if obst in USABLES:
         return ACTIONS[USABLES.index(obst)]
     if obst not in USABLES:
