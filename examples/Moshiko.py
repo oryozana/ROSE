099b7b3084d898620dd13096ad34c4d99c71b57f
BuildTools <unconfigured@null.spigotmc.org>
2022-07-13 14:10:34 +0300

    Please enter the commit message for your changes. Lines starting aa

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -6,12 +6,20 @@ driver_name = "Moshiko Boozaglo"
 
 # def use(x, y, front, obstacle0, obstacle1, obstacle2, obstacle, use_action):
 
-
+USABLES = [obstacles.PENGUIN, obstacles.WATER, obstacles.CRACK]
+ACTIONS = [actions.PICKUP, actions.BRAKE, actions.JUMP]
 def drive(world):
+    
     x = world.car.x
     y = world.car.y
-    obstacle = world.get((x, y - 1))
-    if obstacle == obstacles.PENGUIN:
-        return actions.PICKUP
-    return actions.NONE
+    obst = world.get(x, y - 2)
+    if obst in USABLES:
+        return ACTIONS[USABLES.index(obst)]
+    if obst not in USABLES:
+        if x % 3 == 0:
+            return actions.RIGHT
+        if x % 3 == 1:
+            return actions.LEFT
+        return actions.LEFT
+    
     
