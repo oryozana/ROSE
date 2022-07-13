5f911dea2be41a8d9fdd680e7d64a796905fa849
BuildTools <unconfigured@null.spigotmc.org>
2022-07-13 16:27:19 +0300

    Hello

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -18,10 +18,19 @@ def drive(world):
     layer1 = world.get((x, y - 1))
     layer2 = [world.get((0, y - 2)), world.get((1, y - 2)), world.get((2, y - 2))]
     layer3 = [world.get((0, y - 3)), world.get((1, y - 3)), world.get((2, y - 3))]
+    
+    if layer1 in UNUSABLES:
+        if x % 3 == 2:
+            return actions.LEFT
+        if x % 3 == 0:
+            return actions.RIGHT
+        if layer2[2] in USABLES:
+            return actions.RIGHT
+        return actions.LEFT
+    
     if layer1 in USABLES:
         return ACTIONS[USABLES.index(layer1)]
    
-    
     for pickup in layer2:
         if pickup in USABLES:
             if abs((x % 3) - layer2.index(pickup)) == 1: # layer2.index(pickup) represents the x value of the pickup
@@ -42,11 +51,4 @@ def drive(world):
                 if x % 3 == 2:
                     return actions.LEFT
                 
-    if layer1 in UNUSABLES:
-        if x % 3 == 2:
-            return actions.LEFT
-        if x % 3 == 0:
-            return actions.RIGHT
-        return actions.LEFT
     
-    return actions.NONE
