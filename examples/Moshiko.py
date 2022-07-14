ce2010fed98a7cebe9fef70b852f144289e03c1d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-14 15:04:48 +0300

    Hello

--- examples/Moshiko.py
+++ examples/Moshiko.py
@@ -13,16 +13,16 @@ UNUSABLES = [obstacles.BARRIER, obstacles.TRASH, obstacles.BIKE]
 ACTIONS = [actions.PICKUP, actions.BRAKE, actions.JUMP]
 def drive(world):
     
-    x = world.car.x
+    x = world.car.x % 3
     y = world.car.y
     layer1 = world.get((x, y - 1))
     layer2 = [world.get((0, y - 2)), world.get((1, y - 2)), world.get((2, y - 2))]
     layer3 = [world.get((0, y - 3)), world.get((1, y - 3)), world.get((2, y - 3))]
     
     if layer1 in UNUSABLES:
-        if x % 3 == 2:
+        if x == 2:
             return actions.LEFT
-        if x % 3 == 0:
+        if x == 0:
             return actions.RIGHT
     
     if layer1 in USABLES:
@@ -30,10 +30,10 @@ def drive(world):
    
     for pickup in layer2:
         if pickup in USABLES:
-            if abs((x % 3) - layer2.index(pickup)) == 1: # layer2.index(pickup) represents the x value of the pickup
-                if x % 3 == 0:
+            if abs(x - layer2.index(pickup)) == 1: # layer2.index(pickup) represents the x value of the pickup
+                if x == 0:
                     return actions.RIGHT
-                if x % 3 == 2:
+                if x == 2:
                     return actions.LEFT
                 if layer2.index(pickup) == 0:
                     return actions.LEFT
@@ -42,10 +42,10 @@ def drive(world):
     
     for pickup in layer3:
         if pickup in USABLES:
-            if abs((x % 3) - layer3.index(pickup)) == 2:
-                if x % 3 == 0:
+            if abs(x - layer3.index(pickup)) == 2:
+                if x == 0:
                     return actions.RIGHT
-                if x % 3 == 2:
+                if x == 2:
                     return actions.LEFT
                 
     return actions.NONE
